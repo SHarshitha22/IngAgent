61ec4587e96523d575458d9b0da4ad843a355791
Harshitha S <harshitha@gmail.com>
2025-11-30 00:03:27 +0530

    Testing

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
+from github_client import GitHubClient
+from agents import run_ingestion
+
+def main():
+    client = GitHubClient()
+    pr = client.get_pr_details(1)   # later make dynamic
+    run_ingestion(pr)
+
+if __name__ == "__main__":
+    main()
+
 print('hello world')
 # Change for PR trigger
